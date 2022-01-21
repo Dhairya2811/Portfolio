a28e242aa4a5d7e65a189e9d9934edb22088d8f2
Dhairya2811 <sonidhairya2811@gmail.com>
2022-01-20 22:41:21 -0800

    added navigation and homepage

---
+++ portfolio/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navigationbar from './components/Navigationbar/Navigationbar';
+import HomeComponent from './components/HomeComponent/HomeComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigationbar />
+      <HomeComponent />
+    </div>
+  );
+}
+
+export default App;
